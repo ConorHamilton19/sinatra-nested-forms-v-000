e465c1a6fb2256efca5b0d2ab7f7a46855df4927
Conor Hamilton <conhamil19@gmail.com>
2018-08-03 16:56:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do 
+      
+      erb :new 
+      
+    end 
+    
+    post "/show" do 
+      
+      erb : show
+    end 
 
   end
 end
